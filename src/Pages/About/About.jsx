97a9b44408a76c26e2da59cb6c4df5cfa73d0458
Kortsa <leoklomegah8@gmail.com>
2024-10-01 17:22:12 +0000

    worked on about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,12 +1,53 @@
-import "../About/About.css"
+import "../About/About.css";
 const About = () => {
   return (
     <div className="about-contents">
       <h2>About Us</h2>
+      <h1>
+        Welcome to <span>EventFlex</span>
+      </h1>
       <p>
-        We are dedicated to bringing the community together through local events
-        and activities.
+        Your go-to platform for discovering and participating in local events.
       </p>
+
+      <div className="cards">
+        <div className="card">
+          <div className="card-top">Our mission</div>
+          <p>
+            We envision a community where everyone feels informed, connected,
+            and engaged by providing a one-step platform for community events,
+            we aim to increase participation, promote local initiatives, and
+            bring people closer.
+          </p>
+        </div>
+        <div className="card white">
+          <div className="card-top">Our Vision</div>
+          <p>
+            We envision a community where everyone feels informed, connected,
+            and engaged by providing a one-step platform for community events,
+            we aim to increase participation, promote local initiatives, and
+            bring people closer.
+          </p>
+        </div>
+        <div className="card white">
+          <div className="card-top">What we offer</div>
+          <p>
+            We envision a community where everyone feels informed, connected,
+            and engaged by providing a one-step platform for community events,
+            we aim to increase participation, promote local initiatives, and
+            bring people closer.
+          </p>
+        </div>
+        <div className="card ">
+          <div className="card-top">Join Us</div>
+          <p>
+            We envision a community where everyone feels informed, connected,
+            and engaged by providing a one-step platform for community events,
+            we aim to increase participation, promote local initiatives, and
+            bring people closer.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
